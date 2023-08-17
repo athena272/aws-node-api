5826480b9ee7a5aee872b4e3359627be5632bd9c
athena272 <guilhermera272@gmail.com>
2023-08-17 18:56:56 -0300

    Update user.routes.js

--- src/user.routes.js
+++ src/user.routes.js
@@ -1,15 +1,15 @@
 import { Router } from "express"
 import { UserRepository } from "./repository/UserRepository.js"
 
-const userRout = Router()
+const userRouter = Router() 
 
 const userRepository = new UserRepository()
 
-userRout.post('/users', (req, res) => {
+userRouter.post('/users', (req, res) => { 
     const { cpf, name, birthDate } = req.body
 
     userRepository.create({ cpf, name, birthDate })
     return res.status(200).send()
 })
 
-export { userRout }
+export { userRouter } 
