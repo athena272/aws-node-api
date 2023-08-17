7647918809f5c52301c74a4a1505b1dd8a0d65cb
athena272 <guilhermera272@gmail.com>
2023-08-17 18:26:53 -0300

    Update server.js

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import { userRout } from './user.routes.js'
+
+const app = express()
+
+app.use(express.json())
+
+app.use('/users', userRout)
+
+
+app.listen(3000, () => console.log('🔥 server on fire 🔥'))
