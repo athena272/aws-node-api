65fe3da71f5c4d4fd99524b8abd7e44ac335bb15
athena272 <guilhermera272@gmail.com>
2023-08-20 21:43:58 -0300

    Update users.js

--- src/mocks/users.js
+++ src/mocks/users.js
@@ -1,49 +1,49 @@
 const users = [
     {
         id: 1,
-        cpf: "XXXXXXXX",
+        cpf: 12345678900,
         name: "Guilher R",
         birthDate: "2002-07-02"
     },
     {
         id: 2,
-        cpf: "YYYYYYY",
+        cpf: 98765432100,
         name: "Esmeralda",
         birthDate: "2002-09-09"
     },
     {
         id: 3,
-        cpf: "AAAAAAA",
+        cpf: 11122233300,
         name: 'Maria',
-        birthDate: "1998-04-15" 
+        birthDate: "1998-04-15"
     },
     {
         id: 4,
-        cpf: "BBBBBBBBBB",
+        cpf: 44455566600,
         name: 'Pedro',
-        birthDate: "1985-11-30" 
+        birthDate: "1985-11-30"
     },
     {
         id: 5,
-        cpf: "ZZZZZZZ",
+        cpf: 77788899900,
         name: "João",
         birthDate: "1995-03-15"
     },
     {
         id: 6,
-        cpf: "WWWWWWW",
+        cpf: 00011122200,
         name: "Ana",
         birthDate: "1989-11-25"
     },
     {
         id: 7,
-        cpf: "VVVVVVV",
+        cpf: 33344455500,
         name: "Lucas",
         birthDate: "2000-06-18"
     },
     {
         id: 8,
-        cpf: "UUUUUUU",
+        cpf: 66677788800,
         name: "Carla",
         birthDate: "1993-08-10"
     }
